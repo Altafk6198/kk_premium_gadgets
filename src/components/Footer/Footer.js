3bb9abb29c234290190c68e294dd27d1e46befa1
Altaf Khan <altafk0278@gmail.com>
2023-12-21 19:18:36 +0530

    third coomit by altaf

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,192 @@
 import React from 'react'
 import '../Footer/Footer.css'
+import logo from "../Navbar/kk-logo.png"
 
 const Footer = () => {
   return (
-    <div className='main_footer'>Footer</div>
+    <div className='main_footer'>
+      <footer id="dk-footer" class="dk-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-lg-4">
+              <div class="dk-footer-box-info">
+                <a href="/" class="footer-logo">
+                  <img src={logo} alt="footer_logo" class="img-fluid logo1" />
+                </a>
+                <p class="footer-info-text">
+                  Explore a world of innovation at our gadgets shop. Discover cutting-edge technology, stylish accessories, and smart solutions for your everyday needs. Elevate your lifestyle with the latest gadgets – where innovation meets convenience. Shop now for a seamless blend of functionality and style.</p>
+                <div class="footer-social-link">
+                  <h3>Follow us</h3>
+                  <ul>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-google-plus"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-linkedin"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                {/* End Social link */}
+              </div>
+              {/* End Footer info */}
+              <div class="footer-awarad">
+                <img src={logo} className='logo' alt="icon" />
+                <p>KK Premium Gadgets</p>
+              </div>
+            </div>
+            {/* End Col */}
+            <div class="col-md-12 col-lg-8">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="contact-us">
+                    <div class="contact-icon">
+                      <i class="fa fa-map-o" aria-hidden="true"></i>
+                    </div>
+                    {/* End contact Icon */}
+                    <div class="contact-info">
+                      <h3>Opposite to D C Model, Railway Station Road, Ferozepur Cantt,</h3>
+                      <p>Punjab, India 152001</p>
+
+                    </div>
+                    {/* End Contact Info */}
+                  </div>
+                  {/* End Contact Us */}
+                </div>
+                {/* End Col */}
+                <div class="col-md-6">
+                  <div class="contact-us contact-us-last">
+                    <div class="contact-icon">
+                      <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+                    </div>
+                    {/* End contact Icon */}
+                    <div class="contact-info">
+                      <h3>8146655564</h3>
+                      <h3>8728045600</h3>
+                      <p>Give us a call</p>
+                    </div>
+                    {/* End Contact Info */}
+                  </div>
+                  {/* End Contact Us */}
+                </div>
+                {/* End Col */}
+              </div>
+              {/* End Contact Row */}
+              <div class="row">
+                <div class="col-md-12 col-lg-6">
+                  <div class="footer-widget footer-left-widget">
+                    <div class="section-heading">
+                      <h3>Useful Links</h3>
+                      <span class="animate-border border-black"></span>
+                    </div>
+                    <ul>
+                      <li>
+                        <a href="#">About us</a>
+                      </li>
+                      <li>
+                        <a href="#">Services</a>
+                      </li>
+                      <li>
+                        <a href="#">Projects</a>
+                      </li>
+                      <li>
+                        <a href="#">Our Team</a>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li>
+                        <a href="#">Contact us</a>
+                      </li>
+                      <li>
+                        <a href="#">Blog</a>
+                      </li>
+                      <li>
+                        <a href="#">Testimonials</a>
+                      </li>
+                      <li>
+                        <a href="#">FAQ</a>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* End Footer Widget */}
+                </div>
+                {/* End col */}
+                <div class="col-md-12 col-lg-6">
+                  <div class="footer-widget">
+                    <div class="section-heading">
+                      <h3>Subscribe</h3>
+                      <span class="animate-border border-black"></span>
+                    </div>
+                    <p> Don’t miss to subscribe to our new feeds, kindly fill the form below.
+                      Reference site about all the updated gadgets from KK Premiums, giving information on its origins, as well.</p>
+                    <form action="#">
+                      <div class="form-row">
+                        <div class="col dk-footer-form">
+                          <input type="email" class="form-control" placeholder="Email Address" />
+                          <button type="submit">
+                            <i class="fa fa-send custom-send-icon"></i>
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                    {/* End form */}
+                  </div>
+                  {/* End footer widget */}
+                </div>
+                {/* End Col */}
+              </div>
+              {/* End Row */}
+            </div>
+            {/* End Col */}
+          </div>
+          {/* End Widget Row */}
+        </div>
+        {/* End Contact Container */}
+
+
+        <div class="copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <span>Copyright © 2019, All Right Reserved Altaf_Khan & SmartX Solution Team</span>
+              </div>
+              <div class="col-md-6">
+                <div class="copyright-menu">
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">Terms</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,22 +6,36 @@ import Logo from './kk-logo.png';
 import '../Navbar/Navbar.css'
 
 function Navbar1() {
-    return (
-      <Navbar expand="lg" className="bg-body-tertiary navcolor">
-        <Container>
-          <Navbar.Brand href="#home"><img src={Logo} className='logo' alt='logo'/></Navbar.Brand>
+  return (
+      <Navbar expand="lg" className="bg-body-tertiary navcolor main">
+      <Container >
+        <section className='navlogo'>
+          <Navbar.Brand href="#home"><img src={Logo} className='logo' alt='logo' /></Navbar.Brand>
+        </section>
+        <section className='tab'>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto tab">
+            <Nav className="me-auto">
               <Nav.Link className='tab1' href="#home" >Home</Nav.Link>
               <Nav.Link className='tab1' href="#link">About</Nav.Link>
               <Nav.Link className='tab1' href="#home" >Services</Nav.Link>
-              <Nav.Link className='tab1' href="#link">Contact US</Nav.Link><Nav.Link className='tab1' href="#home" >Home</Nav.Link>
-              <Nav.Link className='tab1' href="#link">Social Media</Nav.Link>
+              <Nav.Link className='tab1' href="#link">Contact</Nav.Link>
+              <Nav.Link className='tab1' href="#home" >Home</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-  export default Navbar1;    
+        </section>
+        <section className='search'>
+          <div class="input-group">
+            <div class="form-outline" className='sbox' data-mdb-input-init>
+              <input type="search" id="form1" class="form-control" placeholder='Search' />
+            </div>
+            <button type="button" class="btn btn-primary sbtn1" data-mdb-ripple-init>
+              <i>Search</i>
+            </button>
+          </div>
+        </section>
+      </Container>
+    </Navbar>
+  );
+}
+export default Navbar1;    
